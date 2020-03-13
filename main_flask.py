de29249161b489c5c777ba6b2a7c05deb19f0db9
DAVID LEE <leeleilei@gmail.com>
2020-03-13 22:50:01 +0800

    read from saved file

--- main_flask.py
+++ main_flask.py
@@ -1,10 +1,16 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, escape, request
-
+from flask import render_template
+from flask import url_for
 import requests
 import simplejson as json
 from requests.adapters import HTTPAdapter
 from json2table import convert
+from datetime import datetime
+
+
+
+
 
 URLS = {'ETF':'http://www.jisilu.cn/data/etf/etf_list/?___jsl=LST___',
         'QDII-T0':'http://www.jisilu.cn/data/qdii/qdii_list/?___jsl=LST___',
@@ -22,11 +28,12 @@ s.mount('http://', HTTPAdapter(max_retries=2))
 s.mount('https://', HTTPAdapter(max_retries=2))
 
 application = Flask(__name__)
+application.debug = True
 
-@application.route('/taoli/')
-def taoli():
 
-    rows={'records':[]}
+@application.route('/api/taoli/')
+def api_taoli():
+    result={'records':[]}
     # 过滤集思录数据
     def _fs_filter(j):
         if ('万手' in j['tradingAmount']):
@@ -41,22 +48,28 @@ def taoli():
 
     for name in URLS: # 按板块
         jsl = s.get(URLS[name]).json()['rows']
-
+        application.logger.info('get {}'.format(URLS[name]))
         jsl = [item['id'] for item in jsl if _jsl_filter(item)]
 
         # 提取fundsmart数据
         for item in jsl: # 提取符合集思录溢价条件的标的
             fs = s.get(TICKER_URL.format(id=item)).json()
+            application.logger.info('get {}'.format(TICKER_URL.format(id=item)))
             if _fs_filter(fs):
                 fs = {HEADERS[x]:fs[x] for x in HEADERS}
-                rows['records'].append(fs)
+                result['records'].append(fs)
+    # dd/mm/YY H:M:S
+    dt_string = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+    result['date'] = dt_string
+    open('taoli.json').write(json.dumps(result))
+    return result
 
-
-    note_html = "<hr><p>说明:拉取集思录ETF、LOF、股票和QDII列表中的所有折溢价超过6%且交易量>10万手的基金的列表</p>"
-    table_attributes = {"border":"1"}
-    tbl_html = convert(rows, table_attributes=table_attributes)
-
-    return tbl_html+note_html
+@application.route('/taoli')
+@application.route('/taoli/')
+def taoli():
+    result = open('taoli.json').read()
+    result.simplejson.loads(result)
+    return render_template('taoli.html', result=result)
 
 if __name__ == "__main__":
-    application.run(debug=True)
+    application.run()
